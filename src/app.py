3d943462ff38b45ec61e1231578c9cb91885fbb4
Jackbringas <102544709+Jackbringas@users.noreply.github.com>
2025-04-09 17:09:01 +0000

    get and delete routes fixed, ininitialized 3 members in datastructures.py

--- src/app.py
+++ src/app.py
@@ -15,6 +15,32 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+# John = {
+# "first_name": "John",
+# "last_name": "Jackson",
+# "age": 33,
+# "lucky_numbers":[7, 13, 22]
+# }
+
+# Jane = {
+# "first_name": "Jane",
+# "last_name": "Jackson",
+# "age": 35,
+# "lucky_numbers":[10, 14, 3]
+# }
+
+
+# Jimmy = {
+# "first_name": "Jimmy",
+# "last_name": "Jackson",
+# "age": 5,
+# "lucky_numbers":[1]
+# }
+
+# jackson_family.add_member(John)
+# jackson_family.add_member(Jane)
+# jackson_family.add_member(Jimmy)
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -25,28 +51,41 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/members', methods=['GET'])
 def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "Message": "Member added successfully",
-        "family": members
-    }
+   
+
+    return jsonify(members), 200
 
 
-    return jsonify(response_body), 200
-
-
-@app.route('/members', methods=['POST'])
+@app.route('/member', methods=['POST'])
 def add_member():
-    body = request.json
-    members = jackson_family.add_member(body)
-
-
-
+    new_member = request.json
+    members = jackson_family.add_member(new_member)
     return jsonify("Member added!!!!"), 200
+
+    
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member_found = jackson_family.get_member(member_id)
+    if not member_found:
+        return jsonify ("Member not found!!"), 400
+    else:
+        return jsonify(member_found), 200
+
+# delete member 
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_family_member(member_id):
+    eliminar_familiar = jackson_family.delete_member(member_id)
+    if not eliminar_familiar:
+        return jsonify({"error": "Member not found!!", "done":eliminar_familiar}), 400
+    else:
+        return jsonify({"message": "Member deleted successfully", "done":eliminar_familiar}), 200
 
 
 
